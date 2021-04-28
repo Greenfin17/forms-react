871f29a5c75611d6b9206554456a000e5f1ea669
Greenfin17 <jmaple@yahoo.com>
2021-04-27 19:47:59 -0500

    adding React Router with navbar

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.scss';
-import StudentForm from '../components/StudentForm';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Routes from '../helpers/Routes';
 import { getStudents } from '../helpers/data/studentData';
-import StudentCard from '../components/StudentCard';
 
 function App() {
   const [students, setStudents] = useState([]);
@@ -12,27 +12,16 @@ function App() {
     getStudents().then((response) => setStudents(response));
   }, []);
 
-  console.warn(students);
-
   return (
-  <div className='App'>
-    <StudentForm formTitle='Add Student'
-    setStudents={setStudents}
-    />
-    <hr />
-    <div className = "card-container">
-    {students.map((studentInfo) => (
-      <StudentCard
-        key={studentInfo.firebaseKey}
-        firebaseKey={studentInfo.firebaseKey}
-        name={studentInfo.name}
-        teacher={studentInfo.teacher}
-        grade={Number(studentInfo.grade)}
-        setStudents={setStudents}
-      />
-    ))}
-    </div>
-  </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes
+          students={students}
+          setStudents={setStudents}
+        />
+      </Router>
+    </>
   );
 }
 
